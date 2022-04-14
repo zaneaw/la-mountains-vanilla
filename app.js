b2483f516d5bff2cae539f3c6691a966e6e01322
Zane Wilson <wilsonzanea@gmail.com>
2022-04-14 11:24:33 -0400

    navbar functionality done

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const buttons = document.querySelectorAll(".climb--tab-button");
 const contents = document.querySelectorAll(".climb--tab-content");
 
 buttons.forEach((button, i) => {
-    button.addEventListener('click', () => {
+    button.addEventListener("click", () => {
         contents.forEach((content) => {
-            content.classList.remove('is-active');
+            content.classList.remove("is-active");
         });
         buttons.forEach((button) => {
-            button.classList.remove('is-active')
+            button.classList.remove("is-active");
         });
-        contents[i].classList.add('is-active');
-        buttons[i].classList.add('is-active');
-    })
-})
+        contents[i].classList.add("is-active");
+        buttons[i].classList.add("is-active");
+    });
+});
+
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector(".nav--container");
+
+    nav.classList.toggle("scrolling-active", window.scrollY > 0);
+});
