d9b965c51ae9b75fa8af33bbb1e4ec2e45d43e3c
Zane Wilson <wilsonzanea@gmail.com>
2022-04-15 11:18:02 -0400

    carousel done

--- app.js
+++ app.js
@@ -5,32 +5,6 @@ window.addEventListener("scroll", () => {
     nav.classList.toggle("scrolling-active", window.scrollY > 0);
 });
 
-// Carousel Component
-const arrows = document.querySelectorAll("[data-carousel-button]");
-const slides = document.querySelector("[data-slides]");
-
-arrows.forEach((arrow) => {
-    arrow.addEventListener("click", () => {
-        // if 'next' arrow clicked, go next, otherwise go back
-        const switchImg = arrow.dataset.carouselButton === "next" ? 1 : -1;
-        const activeSlide = slides.querySelector("[data-active]");
-        // grab index of active slide. switchImg is initially 0.
-        let newIndex = [...slides.children].indexOf(activeSlide) + switchImg;
-
-        /* 
-        When index changes, if it's less than 0, loops to the end of the slides.
-        If it's greater than or equal to the length of the amount of slides, then
-        it will loop back to the first slide.
-        */
-        if (newIndex < 0) newIndex = slides.children.length - 1;
-        if (newIndex >= slides.children.length) newIndex = 0;
-
-        // set the new slide to data-active and delete the old slides data-active attribute
-        slides.children[newIndex].dataset.active = true;
-        delete activeSlide.dataset.active;
-    });
-});
-
 // Tab Components
 const tabs = document.querySelectorAll(".climb__tab-button");
 const contents = document.querySelectorAll(".climb__tab-content");
